25f7b67d758eefdbfdc18d4d041b75ed9e74adee
makkawy16 <yousef.elmakkawy@gmail.com>
2022-10-19 22:11:13 +0200

    finish navigation component

--- app/src/main/java/com/example/postsusingapi/MainActivity.java
+++ app/src/main/java/com/example/postsusingapi/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.postsusingapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.postsusingapi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
     }
 }
